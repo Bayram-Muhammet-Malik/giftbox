a9274eb1aed6bd10f33f79f232dd379760091421
Johan Schaeffer <johan.schaeffer6@etu.univ-lorraine.fr>
2026-04-30 11:04:47 +0200

    [ADD] Ajout de Catégorie + début de Box

---
+++ gift.appli/src/models/Box.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace gift\appli\models;
+use \Illuminate\Database\Eloquent\Model as Eloq;
+
+class Box extends Eloq
+{
+/* TODO */
+}
---
+++ gift.appli/src/models/Categorie.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace gift/appli/models;
+use \Illuminate\Database\Eloquent\Model as Eloq;
+
+class Categorie extends Eloq
+{
+    protected $table = 'categorie';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+// libbélés et description en + dans la db
+    protected $fillable = [
+        'libelle',
+        'description'
+    ];
+
+    public function prestations()
+    {
+        return $this->hasMany(Prestation::class, 'cat_id');
+    }
+
+}

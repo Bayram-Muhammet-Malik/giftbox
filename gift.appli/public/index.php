09abaad95a3957e01862bc79cb5c9a653bf31e7a
Bayram-Muhammet-Malik <muhammet.bayram1@etu.univ-lorraine.frgit config --global user.email muhammet.bayram1@etu.univ-lorraine.frgit config --global user.email muhammet.bayram1@etu.univ-lorraine.fr>
2026-05-04 11:21:12 +0200

    [ADD] Ajout dans le index.php la création de l'app et un premier chargement de route et déclenchement du traitement des requetes (run)

--- gift.appli/public/index.php
+++ gift.appli/public/index.php
@@ -3,4 +3,18 @@ declare(strict_types=1);
 
 session_start();
 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 require_once __DIR__ . '/../src/vendor/autoload.php';
+
+$app = \Slim\Factory\AppFactory::create();
+$app->addRoutingMiddleware();
+$app->setBasePath('/giftbox/gift.appli/public'); // si install dans un sous répertoire
+// Déclaration d’une route :
+$app->get('/hello/{name}', 
+      function(Request $rq, Response $rs,array $args):Response {
+            $rs->getBody()->write("Hello, ". $args['name']);
+            return $rs;
+      }
+);
+$app->run();

c19a7bc2b2996adba520fc7c9a0d3d5d7f9bc1d7
Bayram-Muhammet-Malik <muhammet.bayram1@etu.univ-lorraine.frgit config --global user.email muhammet.bayram1@etu.univ-lorraine.frgit config --global user.email muhammet.bayram1@etu.univ-lorraine.fr>
2026-05-04 11:41:54 +0200

    [ADD] route.php avec le premier get de test qui marche

--- gift.appli/public/index.php
+++ gift.appli/public/index.php
@@ -7,14 +7,9 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 require_once __DIR__ . '/../src/vendor/autoload.php';
 
+
 $app = \Slim\Factory\AppFactory::create();
 $app->addRoutingMiddleware();
 $app->setBasePath('/giftbox/gift.appli/public'); // si install dans un sous répertoire
-// Déclaration d’une route :
-$app->get('/hello/{name}', 
-      function(Request $rq, Response $rs,array $args):Response {
-            $rs->getBody()->write("Hello, ". $args['name']);
-            return $rs;
-      }
-);
+$app = (require_once __DIR__ . '/../src/conf/routes.php')($app);
 $app->run();
---
+++ gift.appli/src/conf/routes.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use \Slim\App;
+require_once __DIR__ . '/../vendor/autoload.php';
+
+return function (App $app): App {
+      $app->get('/hello/{name}', 
+            function(Request $rq, Response $rs,array $args):Response {
+                  $rs->getBody()->write("Hello, ". $args['name']);
+                  return $rs;
+            }
+      );
+      return $app;
+};

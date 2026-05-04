f792514bceb8e59db92790c3c13f2ffb5c5d909e
Johan Schaeffer <johan.schaeffer6@etu.univ-lorraine.fr>
2026-05-04 11:49:34 +0200

    [ADD]Ajout des routes temporairement finies

--- gift.appli/src/conf/routes.php
+++ gift.appli/src/conf/routes.php
@@ -1,14 +1,42 @@
 <?php
 declare(strict_types=1);
+
+// Connexion BD
+use Illuminate\Database\Capsule\Manager as DB;
+$db = new DB;
+$db->addConnection(parse_ini_file(__DIR__ . '/../conf/gift.db.conf.ini'));
+$db->setAsGlobal();
+$db->bootEloquent();
+
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use \Slim\App;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 return function (App $app): App {
-      $app->get('/hello/{name}', 
+      $app->get('/hello/{name}',
             function(Request $rq, Response $rs,array $args):Response {
                   $rs->getBody()->write("Hello, ". $args['name']);
+                  return $rs;
+            }
+      );
+
+      // Catégories
+      $app->get('categories'),
+            function(Request $rq, Response $rs,array $args):Response {
+                  $rs->getBody()->write("Voici la liste des catégories");
+                  // Prendre dans la bd la liste des catégories via requete sql
+                  $categorie = Categories::all();
+
+                  return $rs;
+            }
+      );
+
+      // Prestation : prestation?id=xxxx
+      $app->get('prestation/{id}'),
+            function(Request $rq, Response $rs,array $args):Response {
+                  $rs->getBody()->write("Voici la prestation d'id : " . $args['id']);
                   return $rs;
             }
       );

d8f34263faf293547ba4b4563dedb40d98c8713f
Johan Schaeffer <johan.schaeffer6@etu.univ-lorraine.fr>
2026-05-06 14:40:55 +0200

    [ADD] Question 3 TD3 réalisé, split du index pour creer un bootstrap qui réalise toutes les choses au démarrage et config de l'appli + modif index en conséquence

--- gift.appli/public/index.php
+++ gift.appli/public/index.php
@@ -1,18 +1,9 @@
 <?php
 declare(strict_types=1);
 
-session_start();
-
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
-use \gift\appli\utils\Eloquent;
-
 require_once __DIR__ . '/../src/vendor/autoload.php';
 
-Eloquent::init(__DIR__ . '/../src/conf/gift.db.conf.ini');
+// bootstrap
+$app = require_once __DIR__ . '/../src/conf/bootstrap.php';
 
-$app = \Slim\Factory\AppFactory::create();
-$app->addRoutingMiddleware();
-$app->setBasePath('/giftbox/gift.appli/public'); // si install dans un sous répertoire
-$app = (require_once __DIR__ . '/../src/conf/routes.php')($app);
 $app->run();
---
+++ gift.appli/src/conf/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+session_start();
+
+use \gift\appli\utils\Eloquent;
+
+Eloquent::init(__DIR__ . '/gift.db.conf.ini');
+
+$app = \Slim\Factory\AppFactory::create();
+$app->addRoutingMiddleware();
+$app->setBasePath('/giftbox/gift.appli/public'); // si install dans un sous répertoire
+$app = (require_once __DIR__ . '/routes.php')($app);
+
+return $app;
